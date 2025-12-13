be6d5637750cc96ff6ed46c2f14495db6371b1f3
kavinkumar846 <kevinto7282@gmail.com>
2025-12-13 06:08:15 +0530

    Fix export statement formatting in StudentDashboard

--- components/StudentDashboard.tsx
+++ components/StudentDashboard.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { Upload, FileText, TrendingUp, Calendar, Trophy, Medal, Star, BookOpen } from 'lucide-react';
 import { api } from '../services/api';
